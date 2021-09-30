00cf227572b390505b029d117326b012303319d9
jtricozzi <42682429+jtricozzi@users.noreply.github.com>
2021-09-30 15:42:51 -0400

    Added changing colors and new textures

--- core/src/edu/binghamton/cs/Project3.java
+++ core/src/edu/binghamton/cs/Project3.java
@@ -3,39 +3,76 @@ package edu.binghamton.cs;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Project3 extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
-	float x, y;
-	float dx, dy;
+	Texture img, img2, redC, yellowC, blueC;
+	float x, y, x2, y2;
+	float dx, dy, dx2, dy2;
 	float w, h;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("yellowCircle.png");
+		img2 = new Texture("yellowCircle.png");
+
+		blueC = new Texture("blueCircle.png");
+		redC = new Texture("redCircle.png");
+		yellowC = new Texture("yellowCircle.png");
+
 		x = 0;
 		y = 0;
+		x2 = 5;
+		y2 = 5;
 		dx = 17;
 		dy = 29;
+		dx2= 15;
+		dy2 = 35;
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear((float).2, (float).3, 0, 1);
+		ScreenUtils.clear(255, 255, 255, 1);
 		x = x + dx;
 		y = y + dy;
 
+		x2 = x2 + dx2;
+		y2 = y2 + dy2;
+
 		if((x > w) || (x < 0)){
 			dx = -dx;
+			img = blueC;
 		}
 		if((y > h) || (y < 0)){
 			dy = -dy;
+			img = redC;
 		}
+
+		if((x2 > w) || (x2 < 0)){
+			dx2 = -dx2;
+			img2 = blueC;
+		}
+		if((y2 > h) || (y2 < 0)){
+			dy2 = -dy2;
+			img2 = redC;
+		}
+
+		if(Intersector.overlaps(img, img2)) {
+			dy = -dy;
+			dx = -dx;
+			dy2 = -dy2;
+			dx2 = -dx2;
+			img = yellowC;
+			img2 = yellowC;
+
+		}
+
 		batch.begin();
 		batch.draw(img, x, y);
+		batch.draw(img2, x2, y2);
 		batch.end();
 	}
 	
@@ -43,6 +80,7 @@ public class Project3 extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		img2.dispose();
 	}
 
 	@Override
